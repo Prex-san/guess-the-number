e1f4af00bc07abfe35d7c5cccfdf21d95ffd2070
Prex-san <raghuraman417@gmail.com>
2024-03-25 00:02:33 +0530

    Add better GUI for the game
    After an eternity, I'm finally back to development
    It feels amazing to be back, honestly and I really
    hope that it stays this way from now on. Man, this
    is what I really like to do. Time to fill up the
    git commit history, I guess.

--- script.js
+++ script.js
@@ -1,65 +1,73 @@
-const setMessage = (element, message) => {
-  document.querySelector(element).textContent = message;
-};
+let guessNumber = Math.trunc(Math.random() * 10) + 1;
+const userInput = document.querySelector("#guess");
+const checkPress = document.querySelector("#checkPress");
+const repeatPress = document.querySelector("#repeatPress");
+const highScoreContainer = document.querySelector(".score.high-score");
+const currentScoreContainer = document.querySelector(".score.cur-score");
+const displayNumber = document.querySelector(".num");
+const promptMessage = document.querySelector(".prompt-message");
+console.log(guessNumber);
 
-//State variables
-let num = Math.trunc(Math.random() * 20) + 1;
-let gameState = true;
-let score = 20;
-let highScore = 0;
-setMessage(".cur-score", "Score: " + score);
-
-//Take a guess
-document.querySelector("#checkPress").addEventListener("click", function () {
-  if (gameState) {
-    const guess = Number(document.querySelector("#guess").value);
-    if (score > 1) {
-      //Invalid guess
-      if (!guess) setMessage(".prompt-message", "Enter a valid number!");
-      //Correct guess: GAME WON
-      else if (guess === num) {
-        setMessage(".prompt-message", "You won the game!");
-        setMessage(".num", num);
-        gameState = false;
-        //Set highscore
-        if (score > highScore) highScore = score;
-        setMessage(".high-score", "High Score: " + highScore);
-        //Incorrect guess: SCORE DEDUCTION
-      } else if (guess !== num) {
-        setMessage(".prompt-message", guess > num ? "Go lower!" : "Go Higher!");
-        setMessage(".cur-score", "Score: " + --score);
+let audio = document.querySelector("audio");
+audio.volume = 0.5;
+currentScoreContainer.textContent = 100;
+let attempts = 0;
+let matchState = true;
+checkPress.addEventListener("click", function () {
+  let guess = Number(userInput.value);
+  if (guess) {
+    if (guess > 10) promptMessage.textContent = "The max guess is 10, for now";
+    else {
+      attempts++;
+      if (matchState) {
+        if (guess === guessNumber) {
+          promptMessage.textContent = `Congratulations you guessed it in ${attempts} attempts`;
+          matchState = false;
+          let score = Number(currentScoreContainer.textContent);
+          if (score > Number(highScoreContainer.textContent)) {
+            highScoreContainer.textContent = score;
+          }
+          displayNumber.textContent = guessNumber;
+        } else {
+          scoreCalculator(guess);
+        }
+      } else {
+        promptMessage.textContent = "Press click play again to try again";
       }
-
-      //GAME LOST
-    } else {
-      gameState = false;
-      setMessage(
-        ".prompt-message",
-        "You lost the game! The answer was: " + num
-      );
-      setMessage(".cur-score", "Score: 0");
-      score = 0;
     }
-
-    //Retry despite loss
-  } else {
-    setMessage(".prompt-message", "Press play again to try again");
-    document.querySelector("#checkPress").disabled = true;
-    document.querySelector("#guess").disabled = true;
   }
 });
 
-//Play again
-document.querySelector("#repeatPress").addEventListener("click", function () {
-  num = Math.trunc(Math.random() * 20) + 1;
-  score = 20;
-  gameState = true;
-  setMessage(".cur-score", "Score: " + score);
-  setMessage(".num", "?");
-  setMessage(".prompt-message", "Take a guess");
-  if (document.querySelector("#guess").disabled)
-    document.querySelector("#guess").disabled = false;
-  if (document.querySelector("#checkPress").disabled)
-    document.querySelector("#checkPress").disabled = false;
-  document.querySelector("#guess").value = " ";
+function scoreCalculator(guess) {
+  let score = Number(currentScoreContainer.textContent);
+  if (score > 5) {
+    let scoreReduction =
+      3 * Math.ceil((Math.abs(guessNumber - guess) / 100) * score);
+    score -= scoreReduction;
+    console.log(scoreReduction);
+    currentScoreContainer.textContent = score;
+    let highOrlow = guess > guessNumber ? "lower" : "higher";
+    Math.abs(guessNumber - guess) > 3
+      ? (promptMessage.textContent = `Bad guess, go ${highOrlow}`)
+      : (promptMessage.textContent = `Good guess, go ${highOrlow}`);
+  } else {
+    currentScoreContainer.textContent = 0;
+    matchState = false;
+    promptMessage.textContent = "Game over, good try~";
+  }
+}
+
+repeatPress.addEventListener("click", function () {
+  promptMessage.textContent = "Take a guess!";
+  guessNumber = Math.trunc(Math.random() * 10) + 1;
+  currentScoreContainer.textContent = 100;
+  displayNumber.textContent = "?";
+  matchState = true;
+  attempts = 0;
 });
+
+/*Score formula:
+  Max guess: 10
+  Max point reduction = 10%
+  Score reduction on wrong guess = (Actual number - Guess)/100 * currentScore;
+*/
